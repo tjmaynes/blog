140ff2febd5aac37ab2b856d988eb2769be171f6
TJ Maynes (Personal) <tj@tjmaynes.com>
2020-06-06 23:39:23 -0400

    Add install_dependencies script

--- Makefile
+++ Makefile
@@ -2,16 +2,14 @@ BLOG_DIRECTORY              = $(PWD)
 BLOG_PUBLISHING_DIRECTORY   = $(BLOG_DIRECTORY)/public
 REPO                        = git@github.com:tjmaynes/blog.git
 TARGET_BRANCH               = gh-pages
-HOST                        ?= $(shell hostname -I | awk '{print $1}')
 
 define execute_script
 chmod +x ./scripts/${1}.sh
 ./scripts/${1}.sh
 endef
 
-install_depedencies:
-	git submodule init
-	git submodule update --remote
+install_dependencies:
+	$(call execute_script,$@)
 
 .PHONY: build
 build:
---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+HUGO_VERSION=0.72.0
+
+install_tarred_tool()
+{
+	if [[ -x "$(command -v $1)" ]]; then
+		echo "$1 v$2 already installed..."
+	else
+		echo "Installing $1 v$2 via $3..."
+
+		curl -sL $3 > $1.tar.gz
+		tar -xvf $1.tar.gz --one-top-level=$1 --strip-components 1
+
+		chmod +x $1
+    sudo mv $1 /usr/local/bin
+
+		rm -rf $1.tar.gz
+	fi
+}
+
+install_hugo()
+{
+	install_tarred_tool "hugo" $HUGO_VERSION "https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
+}
+
+install_themes()
+{
+	git submodule init
+	git submodule update --remote
+}
+
+main()
+{
+	install_hugo
+	install_themes
+}
+
+main
